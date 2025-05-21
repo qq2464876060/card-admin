2215dd9745a24f0cd8f77c630980907eb4ebd155
党鹏程 <2464876060@qq.com>
2025-05-21 10:58:43 +0800

    添加 base 配置以支持 GitHub Pages 部署

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/card-admin/', // 👈 仓库名，确保和你的 GitHub 仓库名一致
   plugins: [vue()]
 })
